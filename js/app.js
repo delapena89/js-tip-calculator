0440508c1db55e3824dbdf3ec7200bc1dcf4ef17
Luis <delapenl@gmail.com>
2015-08-06 09:37:52 -0600

    finished first part of problem

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 
-console.log("tip calculator");
+
+document.getElementById("button").addEventListener('click', function () {
+	var billAmt = document.getElementsByClassName('text')[0].value;
+		document.getElementById('tipHere').innerHTML = calculate(billAmt);
+
+	var tipPercentage = document.getElementsByClassName('text')[0].value;
+		document.getElementById('tipHere').innerHTML = calculate(billAmt);
+
+	function calculate(billAmt){
+	var tipAmt = billAmt * 0.20;
+	return tipAmt.toFixed(2);
+	}
+			
+	});
