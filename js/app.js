1a236c323c3388d6e1ac50a0a2d7a967ec3fe2c3
Luis <delapenl@gmail.com>
2015-08-09 11:14:04 -0600

    updated functionality and refactored code

--- js/app.js
+++ js/app.js
@@ -1,15 +1,29 @@
 
+var elementArray = document.getElementsByClassName('text');
 
+// // Added Event Listener to the Button, listening for click.
 document.getElementById("button").addEventListener('click', function () {
-	var billAmt = document.getElementsByClassName('text')[0].value;
-		document.getElementById('tipHere').innerHTML = calculate(billAmt);
 
-	var tipPercentage = document.getElementsByClassName('text')[0].value;
-		document.getElementById('tipHere').innerHTML = calculate(billAmt);
+// 	// Returns an array of HTML elements that contain the class name of 'text'
+	var elementArray = document.getElementsByClassName('text');
+// 	// Returns the first element of the elementArray (bill amount input)
+	var billAmtInput = elementArray[0];
+// 	// Returns the user inputed value
+	var billAmt = parseInt(billAmtInput.value);
+	console.log(billAmt);
 
-	function calculate(billAmt){
-	var tipAmt = billAmt * 0.20;
-	return tipAmt.toFixed(2);
-	}
-			
+	var tipPercentageInput = elementArray[1];
+	// needs refactoring to remove '%'
+	var tipPercentArray = (tipPercentageInput.value).split('').slice(0,2).join("");
+	console.log(tipPercentageInput.value);
+
+	var total = billAmt * (parseInt(tipPercentArray)/100);
+	// Appending total back to the DOM
+	document.getElementById("tipHere").innerHTML = ("You should tip $ " + total.toFixed(2)); 
+
+
 	});
+
+	
+
+	
